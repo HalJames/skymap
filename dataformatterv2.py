fc2b5b5735f6bbbab419f51e93e98b692d2fb383
Eforberger <elsaforberger@icloud.com>
2017-06-20 13:16:59 -0500

    Add files via upload

---
+++ dataformatterv2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 12:21:13 2017
+
+@author: Elsa Forberger
+"""
+
+        
+class Point(object):
+    def __init__(self,dec, ra, err):
+        self.err = err
+        self.ra = ra
+        self.dec = dec
+        if float(self.err) > 2:
+            self.eventType = "Shower"
+        else:
+            self.eventType = "Track"
+    def toString(self):
+        return "dec " + str(self.dec) + " err " + str(self.err) + " type " + self.eventType
+
+out = open("output.txt", "w")
+data = open('/Users/Patron/Documents/upgoing_events.txt', "r+")
+datalist = data.readlines()
+for item in datalist:
+    words = item.split()
+    if words[0] != "MDJ" and words[2] != "ang_err":
+        out.write(Point(words[4], words[3], words[2]).toString() + "\n")
+    #print(str(words))
+    #dec, ra, err, type OUTPUT
+    #mdj, log, err, ra, dec INPUT
+    #print ("MADE IT TO 26")
+data.close()
+out.close()
+        
